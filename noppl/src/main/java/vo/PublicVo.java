9a63ad34e2a02eef6d10ac0c82fc5f8feff8abaf
YuJunCheol <sinaburo0425@gmail.com>
2021-02-27 06:55:43 +0900

    20200227

---
+++ noppl/src/main/java/vo/PublicVo.java
@@ -0,0 +1,15 @@
+package vo;
+
+public class PublicVo {
+	int pageNo;
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	
+	
+}
